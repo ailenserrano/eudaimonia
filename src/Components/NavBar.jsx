4c3709da720fe9ce5df5b1a0daf788216a87b7d6
Ailen Serrano Iglesias <ailenmserrano@gmail.com>
2022-03-01 21:23:34 -0300

    modificacion navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,35 +1,39 @@
 import React from "react";
 
+import logo from "../eudaimoniaLogoERound.png";
+
 function NavBar() {
   return (
     <div>
-      <nav className="navbar navbar-light">
-        <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            <img src="../eudaimoniaLogoERound.png" alt="Eudaimonia logo" />
+            <img src={logo} alt="Eudaimonia logo" width={100} height={100} />
           </a>
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Conjuntos
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Corpiños
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Bombachas
-              </a>
-            </li>
-          </ul>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Conjuntos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Corpiños
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Bombachas
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
